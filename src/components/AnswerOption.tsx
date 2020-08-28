678d40f6c5568e5a063eaa9938503c697366011f
kagafon <kagafon@users.noreply.github.com>
2020-08-28 21:30:16 +0300

    feat: add win/fail sounds

--- src/components/AnswerOption.tsx
+++ src/components/AnswerOption.tsx
@@ -5,7 +5,7 @@ import { selectOption } from 'store/root-redux';
 import { QuizState } from 'store/root-reducer';
 import { IAnswer } from './commonTypes';
 import { ListGroupItem, ListGroup } from 'react-bootstrap';
-// import useSound from 'use-sound';
+import useSound from 'use-sound';
 
 export interface AnswerOptionProps {
   answer: IAnswer;
@@ -17,11 +17,19 @@ export const AnswerOption = ({ answer }: AnswerOptionProps): JSX.Element => {
     (state) => state.checkedState[answer.id]
   );
   // const [play] = useSound('assets/audio/hover.ogg', { volume: 0.5 });
+  const [playFail] = useSound('assets/audio/fail.mp3', { volume: 1 });
+  const [playWin] = useSound('assets/audio/win.mp3', { volume: 1 });
 
   const onClick = React.useCallback(() => selectOption(dispatch, answer), [
     answer,
     dispatch,
   ]);
+
+  React.useEffect(() => {
+    if (checkResult !== undefined) {
+      (checkResult ? playWin : playFail)();
+    }
+  }, [checkResult, playWin, playFail]);
 
   if (checkResult === undefined)
     return <ListGroup.Item onClick={onClick}>{answer.name}</ListGroup.Item>;
